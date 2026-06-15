832e8ecd6530bf6950d1295ec1fbe0d7da4a3374
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-06-15 09:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let level = 1;
+function getDOMlevel(element){
+    while(element.parentElement){
+        level++;
+        element = element.parentElement;
+    }
+    return level;
+}
+
+let element = document.getElementById('level');
+let n = getDOMlevel(element);
+alert('The level of the element is: ' + n);
